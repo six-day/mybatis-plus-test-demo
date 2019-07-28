8b8eb4aafbd577ba7897179fa22458919f32fc61
huangchengxiang <aa453654842@qq.com>
2019-07-28 14:57:14 +0800

    基本

---
+++ src/main/java/com/example/demo/test/SampleTest1.java
@@ -0,0 +1,28 @@
+package com.example.demo.test;
+
+import com.example.demo.dao.UserDao;
+import com.example.demo.entity.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleTest1 {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userDao.selectList(null);
+        Assert.assertEquals(5, userList.size());
+        userList.forEach(System.out::println);
+    }
+}
